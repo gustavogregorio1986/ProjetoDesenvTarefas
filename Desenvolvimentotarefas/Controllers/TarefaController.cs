9d726b3c50cbce58be17bff3de5af7200331b1d5
guga56 <gustavogregorio086@gmail.com>
2023-07-19 14:59:04 -0300

    layout do sistema de tarefas

---
+++ Desenvolvimentotarefas/Controllers/TarefaController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Desenvolvimentotarefas.Controllers
+{
+    public class TarefaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult ApagarConfirmacao()
+        {
+            return View();
+        }
+
+        public IActionResult Apagar()
+        {
+            return View();
+        }
+    }
+}
